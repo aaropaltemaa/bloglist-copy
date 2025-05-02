ca3c659c9ca0dd5552f1fd0c9b923a036473e32b
Aaro Paltemaa <apaltemaa@gmail.com>
2025-05-02 14:02:00 +0300

    catch-all route modified

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(express.static("dist"));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 
